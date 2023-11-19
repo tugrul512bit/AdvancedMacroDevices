f12855036df53c325cc90978ec21b579de091ef4
Hüseyin Tuğrul büyükışık <huseyin.tugrul.buyukisik@gmail.com>
2023-11-19 15:25:15 +0300

    hello world

---
+++ World.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "Class.h"
+#include<vector>
+#include<memory>
+class Factory
+{
+private:
+	int _ownerId;
+	int _capacity;
+};
+class World
+{
+public:
+private:
+	std::vector<CharacterClass> _vendors;
+	std::vector<CharacterClass> _consumers;
+	std::vector<Factory> _factories;
+};
